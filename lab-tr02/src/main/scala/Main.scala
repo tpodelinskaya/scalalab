368787993849df4568535b6cdd6eb745e6c4b943
savchenkoV <79515414890@yandex.ru>
2021-10-23 13:56:31 +0300

    moved the code from TestFilter to Main

--- lab-tr02/src/main/scala/Main.scala
+++ lab-tr02/src/main/scala/Main.scala
@@ -1,6 +1,17 @@
+import org.apache.spark.sql.SparkSession
 
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello, I'm TR02!")
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("TestFilter")
+      .getOrCreate()
+    val sc = spark.sparkContext
+    val rdd = sc.textFile("src/main/scala/Text.txt")
+    val filterRDD = rdd.filter(x => x.startsWith("<") && x.endsWith(">"))
+    val count = filterRDD.count();
+    println(count);
   }
 }
