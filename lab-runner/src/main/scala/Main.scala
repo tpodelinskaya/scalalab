4383950434d77d2a54bd075b38611ddecdfe8c4f
RedCrabVb <cany245@mail.ru>
2021-10-27 08:34:44 +0300

    Add parameter to run command

--- lab-runner/src/main/scala/Main.scala
+++ lab-runner/src/main/scala/Main.scala
@@ -1,18 +1,49 @@
-import com.google.gson.JsonParser
+import com.google.gson.{JsonElement, JsonParser}
 
 import java.io.FileReader
-import sys.process._
+import scala.sys.process._
 
 object Main {
+
   def main(args: Array[String]): Unit = {
     println("Hello! I'm Runner!")
 
-    val jsonConf = new JsonParser().parse(new FileReader(args(0))).getAsJsonObject
-    println("input config: " + jsonConf)
-    val sparkCommand = (f"/home/redcrab/Public/spark/bin/spark-submit --class ${jsonConf.get("class").getAsString} " +
-      f"${jsonConf.get("jar-file-path").getAsString} " +
-      f"inpath=/home/redcrab/test=dir mask=*.txt outpath=./out_path").!
+    implicit def anyRefToString(any: AnyRef) = any.toString
 
-    sparkCommand
+    implicit def jsonElementGetString(json: JsonElement) = json.getAsString
+
+
+    val jsonConf = new JsonParser().parse(new FileReader(args(0))).getAsJsonObject
+    val sparkSubmit = if (args.length == 2) args(1) else "spark-submit"
+
+    val classMain: String = jsonConf.get("class")
+    val appName = jsonConf.get("app-name").toString //right
+    val jarFile: String = jsonConf.get("jar-file-path")
+
+    val sparkParams = jsonConf.get("spark-params").getAsJsonObject
+    val master = sparkParams.get("master") //Call error
+    val deployMode = sparkParams.get("deploy-mode") //Call error
+    val executorCores: String = sparkParams.get("executor-cores")
+    val executorMemory: String = sparkParams.get("executor-memory")
+    val driverMemory: String = sparkParams.get("driver-memory")
+
+    val trParamsJson = jsonConf.get("tr-params").getAsJsonObject
+    val trParams = trParamsJson.keySet().toArray().map(x => {
+      s"$x=${trParamsJson.get(x).getAsString}"
+    }).mkString(" ")
+
+
+    val command =
+      f"""$sparkSubmit
+          --name $appName
+         --executor-memory $executorMemory
+         --driver-memory $driverMemory
+         --executor-cores $executorCores
+         --class $classMain
+         ${jarFile} $trParams""".stripMargin
+
+    println(command)
+
+    (command).!
   }
 }
