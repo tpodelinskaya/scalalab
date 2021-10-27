2c97c6b16ad88e575bd11623caaf20bf03d3ed04
savchenkoV <79515414890@yandex.ru>
2021-10-27 10:30:09 +0300

    Add code

--- lab-tr02/src/main/scala/Main.scala
+++ lab-tr02/src/main/scala/Main.scala
@@ -1,18 +1,47 @@
 import org.apache.spark.sql.SparkSession
 
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello, I'm TR02!")
 
-    val spark = SparkSession.builder()
+    val inpath = "E:\\Учёба!\\Scala\\new_project\\scalalab\\lab-tr02\\src\\main\\scala\\Simple.html"
+
+    transformation(inpath)
+  }
+
+  def transformation(inpath: String) = {
+
+    val spark = SparkSession
+      .builder()
       .master("local[*]")
-      .appName("TestFilter")
+      .appName("Spark TR02")
       .getOrCreate()
 
+    import spark.sqlContext.implicits._
+
     val sc = spark.sparkContext
-    val rdd = sc.textFile("src/main/scala/Text.txt")
-    val filterRDD = rdd.filter(x => x.startsWith("<") && x.endsWith(">"))
-    val count = filterRDD.count();
-    println(count);
+
+    val readFile = sc.textFile(inpath)
+
+    val words = readFile
+      .flatMap(lines => lines.split(" "))
+      .filter(word => word.contains("<") && word.contains (">"))
+    val counts = words
+      .map(w => (w, 1))
+      .reduceByKey(_ + _)
+
+    val columns = Seq("Tag", "Count")
+
+    val df = counts.toDF(columns: _*)
+
+    df.printSchema()
+    df.show()
+
+    //df.write.option("header", true).mode("append").csv("E:\\Учёба!\\Scala\\new_project\\scalalab\\lab-tr02\\src\\main\\scala\\new.csv")
+
+    spark.stop()
+
+
   }
 }
