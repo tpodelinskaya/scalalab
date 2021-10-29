2a91d03058d2a32ab48175dc1723bdae1893897f
RedCrabVb <cany245@mail.ru>
2021-10-29 15:33:22 +0300

    Fix warnings

--- lab-tr03/src/main/scala/Main.scala
+++ lab-tr03/src/main/scala/Main.scala
@@ -6,7 +6,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hello, I'm TR03!")
       
-    val keyValPattern: Regex = "^([\\w]+)=([\\w=\\*\\.\\/ ]+)$".r;
+    val keyValPattern: Regex = "^([\\w]+)=([\\w=*./ ]+)$".r
     val map = args.map(arg => {
           val keyVal = keyValPattern.findPrefixMatchOf(arg).getOrElse(throw new RuntimeException("not valid arg"))
           (keyVal.group(1), keyVal.group(2))
@@ -14,20 +14,20 @@ object Main {
 
     println(s"input data: $map")
 
-    val inpath = map.get("inpath").getOrElse(throw new RuntimeException("not set variable 'inpath'"))
-    val mask = map.get("mask").getOrElse(throw new RuntimeException("not set variable 'mask'"))
-    val outpath = map.get("outpath").getOrElse(throw new RuntimeException("not set variable 'outpath'"))
+    val inpath = map.getOrElse("inpath", throw new RuntimeException("not set variable 'inpath'"))
+    val mask = map.getOrElse("mask", throw new RuntimeException("not set variable 'mask'"))
+    val outpath = map.getOrElse("outpath", throw new RuntimeException("not set variable 'outpath'"))
 
 
     fileTransformation(inpath, mask, outpath)
   }
 
-  def fileTransformation(inpath: String, mask: String, outpath: String) = {
+  def fileTransformation(inpath: String, mask: String, outpath: String): Unit = {
     val spark = SparkSession
       .builder()
       .appName("Java Spark SQL TR03")
       .config("spark.master", "local")
-      .getOrCreate();
+      .getOrCreate()
 
 
     import spark.sqlContext.implicits._
