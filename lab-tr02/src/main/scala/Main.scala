a34e2d05b05f3e5e02977d130d45dca16734aaf8
savchenkoV <79515414890@yandex.ru>
2021-11-02 22:05:39 +0300

    fix code

--- lab-tr02/src/main/scala/Main.scala
+++ lab-tr02/src/main/scala/Main.scala
@@ -5,7 +5,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hello, I'm TR02!")
 
-    val inpath = "E:\\Учёба!\\Scala\\new_project\\scalalab\\lab-tr02\\src\\main\\scala\\Simple.html"
+    val inpath = "E:\\Учёба!\\Scala\\new_project\\scalalab\\lab-tr02\\src\\main\\scala\\Simple1.html"
 
     transformation(inpath)
   }
@@ -25,8 +25,8 @@ object Main {
     val readFile = sc.textFile(inpath)
 
     val words = readFile
-      .flatMap(lines => lines.split(" "))
-      .filter(word => word.contains("<") && word.contains (">"))
+      .flatMap(lines => lines.split("  "))
+      .filter(word => (word.startsWith("<") && word.endsWith(">")))
     val counts = words
       .map(w => (w, 1))
       .reduceByKey(_ + _)
@@ -34,11 +34,10 @@ object Main {
     val columns = Seq("Tag", "Count")
 
     val df = counts.toDF(columns: _*)
+    //df.coalesce(1).write.option("header", true).mode("append").csv("file:///C:/Users/Валерик/Downloads/htmls")
 
-    df.printSchema()
     df.show()
-
-    //df.write.option("header", true).mode("append").csv("E:\\Учёба!\\Scala\\new_project\\scalalab\\lab-tr02\\src\\main\\scala\\new.csv")
+    df.printSchema()
 
     spark.stop()
 
