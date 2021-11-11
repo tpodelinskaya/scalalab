bd1e02fdf16ac7cf7f1a49d46c371dfad315729a
valera <79515414890@yandex.ru>
2021-11-11 11:38:58 +0300

    fix code

--- lab-tr02/src/main/scala/Main.scala
+++ lab-tr02/src/main/scala/Main.scala
@@ -1,16 +1,27 @@
 import org.apache.spark.sql.SparkSession
 
+import scala.util.matching.Regex
+
 
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello, I'm TR02!")
 
-    val inpath = "E:\\Учёба!\\Scala\\new_project\\scalalab\\lab-tr02\\src\\main\\scala\\Simple1.html"
+    val keyValPattern: Regex = "^([\\w]+)=(.+)$".r
+    val map = args.map(arg => {
+      val keyVal = keyValPattern.findPrefixMatchOf(arg).getOrElse(throw new RuntimeException("not valid arg"))
+      (keyVal.group(1), keyVal.group(2))
+    }).toMap
 
-    transformation(inpath)
+    println(s"input data: $map")
+
+    val inpath = map.get("inpath").getOrElse(throw new RuntimeException("not set variable 'inpath'"))
+    val outpath = map.get("outpath").getOrElse(throw new RuntimeException("not set variable 'outpath'"))
+
+    transformation(inpath, outpath)
   }
 
-  def transformation(inpath: String) = {
+  def transformation(inpath: String, outpath: String) = {
 
     val spark = SparkSession
       .builder()
@@ -25,8 +36,9 @@ object Main {
     val readFile = sc.textFile(inpath)
 
     val words = readFile
-      .flatMap(lines => lines.split("  "))
-      .filter(word => (word.startsWith("<") && word.endsWith(">")))
+      .flatMap(lines => lines.split("\\s.*?\\s"))
+      .filter(word => word.matches("\\<.*?\\>"))
+
     val counts = words
       .map(w => (w, 1))
       .reduceByKey(_ + _)
@@ -35,13 +47,11 @@ object Main {
 
     val df = counts.toDF(columns: _*)
 
-    df.coalesce(1).write.option("header", true).mode("append").csv("file:///C:/Users/Валерик/Downloads/htmls")
+    df.coalesce(1).write.option("header", true).mode("append").csv(outpath)
 
     df.show()
     df.printSchema()
 
     spark.stop()
-
-
   }
 }
