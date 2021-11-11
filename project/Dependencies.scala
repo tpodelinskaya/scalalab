eb5664d3aca4e17310f737e4b026768834e2cd85
valera <79515414890@yandex.ru>
2021-11-11 11:38:31 +0300

    update version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,13 +6,13 @@ object Dependencies {
 
   private object Version {
     val spark              = "2.4.8"
-    val sparkSQL           = "3.0.0"
+    val sparkSQL           = "2.4.8"
   }
 
   object Spark {
 
-    val Core =  "org.apache.spark" %% "spark-core" % Version.spark
-    val Sql = "org.apache.spark" %% "spark-sql" % Version.sparkSQL
+    val Core =  "org.apache.spark" %% "spark-core" % Version.spark % "provided"
+    val Sql = "org.apache.spark" %% "spark-sql" % Version.sparkSQL % "provided"
 
 
   }
