f1f4500e2961f18e6778092158f8e6d708752409
sav4enkoVV <92573428+sav4enkoVV@users.noreply.github.com>
2021-12-10 09:59:57 +0300

    Update Main.scala

--- lab-tr02/src/main/scala/Main.scala
+++ lab-tr02/src/main/scala/Main.scala
@@ -14,8 +14,6 @@ object Main {
 
     println(s"Input data: $argsMap")
 
-    //Что будет, если входного файла не существует или нет прав на запись в дирукторию?
-    //Как мы это поймем, запуская наш код через раннер?
     val inpath = getArg(argsMap, "inpath")
     val outpath = getArg(argsMap, "outpath")
 
@@ -36,10 +34,6 @@ object Main {
 
   def transformation(inpath: String, outpath: String) = {
 
-    //Как запускается spark - локально или на сервере тоже вынести в настройки
-    //Наименование приложения вынести в настройки
-    //И то и то будет в конфигурационном файле json, а значит, может быть подставленно раннером
-    //И уже забрано трансформацией внутри себя
     val spark = SparkSession
       .builder()
       .master("local[*]")
